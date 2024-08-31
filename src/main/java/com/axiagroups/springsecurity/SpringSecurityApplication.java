e86712dbfcda902b2634271db477b41c1394880b
Ahsan Habib <ict.wsd19@waltonplc.com>
2024-08-31 12:25:55 +0600

    first commit

--- src/main/java/com/axiagroups/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/axiagroups/springsecurity/SpringSecurityApplication.java
@@ -2,7 +2,7 @@ package com.axiagroups.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// test
 @SpringBootApplication
 public class SpringSecurityApplication {
 
