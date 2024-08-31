31bfd11301a3971fcedb9a9db35b5aa30570a181
Ahsan Habib <ict.wsd19@waltonplc.com>
2024-08-31 12:24:18 +0600

    first commit

---
+++ src/test/java/com/axiagroups/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axiagroups.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
